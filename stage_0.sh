d13200710ca3d12a44f0888e76e6f34883284dae
cata0nana <101589752+cata0nana@users.noreply.github.com>
2022-11-04 20:51:15 +0100

    Update stage_0.sh

--- stage_0.sh
+++ stage_0.sh
@@ -1,14 +1,15 @@
-// REMOVE CRON
+// * REMOVE CRON
 crontab -r
 date >> xxxxxx_test
 
-// REMOVE 0ct0pus_main
+// * REMOVE 0ct0pus_main
 rm -rf 0ct0pus_main
 rm -rf /root/hassed/*
 mkdir -p /root/hassed
 
 echo  $(grep '^sudo:.*$' /etc/group | cut -d: -f4) > /root/hassed/read.me
 
+// * CLONE REPO 
 git clone https://github.com/cata0nana/0ct0pus_main.git
 
 cp 0ct0pus_main/* /root/hassed/
@@ -19,6 +20,9 @@ crontab -r
 
 (crontab -l -u root 2>/dev/null; echo "*/30 * * * * python3 /root/hassed/tel_tel.py") | crontab -
 
+// RESTOR SEVICE CRON
 service cron stop && service cron start
+
+
 python3 /root/hassed/docker_hook.py running_GCS
 
